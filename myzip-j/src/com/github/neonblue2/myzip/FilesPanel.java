c805c3cc050e842f19ed601725261cbe305695f2
James Madley <jdmadley@gmail.com>
2012-11-11 14:44:18 +1030

    Placeholder for the file list coming later.

--- myzip-j/src/com/github/neonblue2/myzip/FilesPanel.java
+++ myzip-j/src/com/github/neonblue2/myzip/FilesPanel.java
@@ -3,11 +3,14 @@ package com.github.neonblue2.myzip;
 import java.awt.BorderLayout;
 
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 public class FilesPanel extends JPanel {
 	private JTextField outputTextField;
+	private JList fileList;
 	
 	public FilesPanel() {
 		setLayout(new BorderLayout());
@@ -19,7 +22,12 @@ public class FilesPanel extends JPanel {
 		outputPanel.add(outputLabel, BorderLayout.WEST);
 		outputPanel.add(outputTextField, BorderLayout.CENTER);
 		
+		String[] s = {"File 1", "File 2", "File 3"};
+		fileList = new JList(s);
+		final JScrollPane fileListScrollPane = new JScrollPane(fileList);
+		
 		add(outputPanel, BorderLayout.NORTH);
+		add(fileListScrollPane, BorderLayout.CENTER);
 		
 	}
 }
